b79923dc84db1c9b683d064b8d7c0b9fa62f8d22
matthewRindner <32372767+matthewRindner@users.noreply.github.com>
2021-05-19 22:25:34 -0400

    Add files via upload

--- src/SortedCollection.java
+++ src/SortedCollection.java
@@ -1,9 +1,13 @@
 //TODO: Import libraries as needed
 import java.util.NoSuchElementException;
+import java.util.*;
+import java.lang.*;
+
+
 
 public class SortedCollection {
 	// TODO: Add member variables or methods as needed
-
+	ArrayList<Integer> list = new ArrayList<Integer>();
 	/**
 	 * Adds the number n to the collection.
 	 * 
@@ -12,6 +16,7 @@ public class SortedCollection {
 	 */
 	public boolean add(int n) {
 		// TODO: Implement
+		list.add(n);
 		return true;
 	}
 
@@ -23,6 +28,13 @@ public class SortedCollection {
 	 */
 	public int remove() throws NoSuchElementException {
 		// TODO: Implement
+		if(list.isEmpty())
+		{
+			throw new NoSuchElementException();
+		}
+
+		//list.sort(Comparator.naturalOrder()); 	//ascending order
+		list.remove(0);
 		return 0;
 	}
 
@@ -33,14 +45,23 @@ public class SortedCollection {
 		System.out.println("Usage: java SortedCollection [num1] [num2] [num3] ...");
 	}
 
+
+
+
+
+
 	/**
 	 * Main method. Receives a list of numbers as commandline arguments and prints
 	 * out the list in sorted order from smallest to largest.
 	 * 
 	 * @param args commandline arguments; see showUsage() for detailed information
 	 */
+
 	public static void main(String[] args) {
 		SortedCollection collection = new SortedCollection();
+
+		
+
 		if (args.length == 0) {
 			showUsage();
 			return;
@@ -48,6 +69,28 @@ public class SortedCollection {
 		
 		// TODO: add numbers in commandline arguments to collection using the add(int) method.
 		// If any commandline argument is not a number, call showUsage() and return.
+
+		for (int i = 0; i < args.length; i++) {
+			try{
+				int input_num = Integer.parseInt(args[i]);
+				//parseInt worked
+				collection.add(input_num);
+				System.out.println("input_num: " + input_num);
+			}
+			catch (NumberFormatException e)
+			{
+				//parseInt failed
+				showUsage();
+				return;
+			}
+		}
+		// System.out.println("Print Arraylist using for each loop");
+		// for( String listNums : list ){
+  //  			System.out.println(listNums);
+		// }
+
+		// list.sort(Comparator.naturalOrder()); 	//ascending order
+
 		
 		System.out.print("sorted: ");
 		for (int i = 0; i < args.length; i++) {
